ff5fc40ceff959c3f2601e71fdd56320620a2e45
Giovanny L. Silva <didiosilva395@gmail.com>
2023-04-17 17:44:28 -0400

    Card detalhe

--- src/pages/Postdetail/index.tsx
+++ src/pages/Postdetail/index.tsx
@@ -1,11 +1,19 @@
 import { MainCard } from "../../components/MainCard"
 import { MainCardDetail } from "../../components/MainCardDetail"
-import { PostDetailMain } from "./styled"
+import { ContentPostDetail, PostDetail, PostDetailMain } from "./styled"
 
 export const Postdetail = () => {
   return (
     <PostDetailMain>
       <MainCardDetail/>
+      <ContentPostDetail>
+          <PostDetail>
+            <p>
+            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
+
+            </p>
+          </PostDetail>
+      </ContentPostDetail>
       
 
 
--- src/pages/Postdetail/styled.ts
+++ src/pages/Postdetail/styled.ts
@@ -3,3 +3,17 @@ import styled from "styled-components";
 export const PostDetailMain = styled.main`
   
 `;
+
+export const ContentPostDetail = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+export const PostDetail = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 864px;
+  height: 100%;
+  padding: 40px;
+`;
